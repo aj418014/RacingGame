0f99457ecd883e2d5409d6192615d9539cdaeae6
Irisedmonson <ie009416@scc-mmlab-15.local>
2019-09-20 19:57:43 -0400

    Timer and checkpoint and finish line

---
+++ RacingGame/Assets/Scripts/WinScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    public GameObject carboi;
+    public int maxLaps;
+    public List<GameObject> places;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Laps>().passedCheckPoint ==true)
+        {
+            if (other.gameObject == carboi && other.gameObject.GetComponent<Laps>().currentLap == maxLaps)
+            {
+                print("Carboi");
+                GameObject.Find("TimeText").GetComponent<TimerScript>().Finish();
+
+                places.Add(other.gameObject);
+
+            }
+            else
+            {
+                other.gameObject.GetComponent<Laps>().currentLap++;
+                other.gameObject.GetComponent<Laps>().passedCheckPoint = false;
+            }
+        }
+
+    }
+}
