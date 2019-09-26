1b546adec96a331c8225fd19a41a6e7143b11a66
Andy <aj418014@ohio.edu>
2019-09-26 19:01:12 -0400

    AI priority was added

--- RacingGame/Assets/AIBehaviour.cs
+++ RacingGame/Assets/AIBehaviour.cs
@@ -4,12 +4,45 @@ using UnityEngine;
 
 public class AIBehaviour : MonoBehaviour
 {
-    public List<Transform> checkpoints;
+    GameObject checkpointParent;
+    public Transform[] checkpoints;
     public Transform currentCheckpoint;
+    public int level;
+    int minPriority;
+    int maxPriority;
     // Start is called before the first frame update
     void Start()
     {
-        currentCheckpoint = checkpoints[0];
+        checkpointParent = GameObject.Find("AI Checkpoints");
+        checkpoints = new Transform[checkpointParent.transform.childCount];
+        for(int i = 0; i < checkpointParent.transform.childCount; i++)
+        {
+            checkpoints[i] = checkpointParent.transform.GetChild(i);
+        }
+        switch (level)
+        {
+            case (1):
+                minPriority = 3;
+                maxPriority = 6;
+                break;
+            case (2):
+                minPriority = 3;
+                maxPriority = 6;
+                break;
+            case (3):
+                minPriority = 2;
+                maxPriority = 5;
+                break;
+            case (4):
+                minPriority = 1;
+                maxPriority = 4;
+                break;
+            case (5):
+                minPriority = 1;
+                maxPriority = 2;
+                break;              
+        }
+        currentCheckpoint = checkpoints[0].GetComponent<ChangeAIGoal>().SetGoal(minPriority, maxPriority);
     }
 
     // Update is called once per frame
@@ -20,16 +53,16 @@ public class AIBehaviour : MonoBehaviour
     }
     public void  IncrementCheckpoint()
     {
-        for(int i = 0; i < checkpoints.Count; i++)
+        for(int i = 0; i < checkpoints.Length; i++)
         {
-            if (i == checkpoints.Count - 1)
+            if (i == checkpoints.Length - 1)
             {
-                currentCheckpoint = checkpoints[0];
+                currentCheckpoint = checkpoints[0].GetComponent<ChangeAIGoal>().SetGoal(minPriority, maxPriority);
                 return;
             }
-            if(currentCheckpoint == checkpoints[i])
+            if(currentCheckpoint.parent == checkpoints[i])
             {
-                currentCheckpoint = checkpoints[i + 1];
+                currentCheckpoint = checkpoints[i + 1].GetComponent<ChangeAIGoal>().SetGoal(minPriority, maxPriority);
                 return;
             }
 
