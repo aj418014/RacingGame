61d71bd4b7a8d56ce2fa4cffbd3865782e9bd355
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-09-29 23:40:00 -0400

    AI update

--- RacingGame/Assets/Scripts/CheckPoint.cs
+++ RacingGame/Assets/Scripts/CheckPoint.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     public List<GameObject> carboi;
-
+    public bool isMainCheckpoint = false;
     private void OnTriggerEnter(Collider other)
     {
         for (int i = 0; i < carboi.Count; i++)
         {
             if (other.gameObject == carboi[i])
             {
-                other.gameObject.GetComponent<Laps>().passedCheckPoint = true;
+                if (isMainCheckpoint == true)
+                {
+                    other.gameObject.GetComponent<Laps>().passedCheckPoint = true;
+                }
                 return;
+                
             }
         }
     }
--- RacingGame/Assets/Scripts/Laps.cs
+++ RacingGame/Assets/Scripts/Laps.cs
@@ -7,5 +7,10 @@ public class Laps : MonoBehaviour
     public int currentLap;
     public bool passedCheckPoint = false;
     public bool hasFinished = false;
-
+    public Transform[] checkpoints;
+    public Transform currentCheckpoint;
+    void ChangeCheckpoint()
+    {
+        for(int i = 0; i < checkpoints.Length)
+    }
 }
