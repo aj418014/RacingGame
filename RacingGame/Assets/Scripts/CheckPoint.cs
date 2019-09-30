79cdc786076a79b21c7623523b9e64d23f5d4858
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-09-29 23:44:42 -0400

    Revert "AI update"
    This reverts commit 61d71bd4b7a8d56ce2fa4cffbd3865782e9bd355.

--- RacingGame/Assets/Scripts/CheckPoint.cs
+++ RacingGame/Assets/Scripts/CheckPoint.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     public List<GameObject> carboi;
-    public bool isMainCheckpoint = false;
+
     private void OnTriggerEnter(Collider other)
     {
         for (int i = 0; i < carboi.Count; i++)
         {
             if (other.gameObject == carboi[i])
             {
-                if (isMainCheckpoint == true)
-                {
-                    other.gameObject.GetComponent<Laps>().passedCheckPoint = true;
-                }
+                other.gameObject.GetComponent<Laps>().passedCheckPoint = true;
                 return;
-                
             }
         }
     }
--- RacingGame/Assets/Scripts/Laps.cs
+++ RacingGame/Assets/Scripts/Laps.cs
@@ -7,10 +7,5 @@ public class Laps : MonoBehaviour
     public int currentLap;
     public bool passedCheckPoint = false;
     public bool hasFinished = false;
-    public Transform[] checkpoints;
-    public Transform currentCheckpoint;
-    void ChangeCheckpoint()
-    {
-        for(int i = 0; i < checkpoints.Length)
-    }
+
 }
